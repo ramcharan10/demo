f913f6fe89ef55c27569e93ffca3d6f2cd5817e6
ramcharan10 <s3775271@rmit.edu.vn>
2021-08-19 19:03:09 +0700

    test2

--- src/test/java/Bank/AccountTest.java
+++ src/test/java/Bank/AccountTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class AccountTest {
 
 	Account acc;
-	//safasdfsdf
+	//safasdfsdfasd
 	@Test
 	public void testGetBalance() {
 		// newly created default account has balance of 0.0
